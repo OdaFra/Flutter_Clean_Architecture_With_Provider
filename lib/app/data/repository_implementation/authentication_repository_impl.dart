af639ef364cfaf4aa3fadb38f63309ed55e34eeb
Oscar Ramirez <osramirezf@gmail.com>
2023-04-07 16:41:19 -0400

    feat: Agregamos class abstracta como su implementacion para validar autenticacion de usuario.

---
+++ lib/app/data/repository_implementation/authentication_repository_impl.dart
@@ -0,0 +1,16 @@
+import '../../domain/models/user.dart';
+import '../../domain/repositories/repositories.dart';
+
+class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  @override
+  Future<User> getUserData() {
+    return Future.value(
+      User(),
+    );
+  }
+
+  @override
+  Future<bool> get isSignedIn {
+    return Future.value(true);
+  }
+}
---
+++ lib/app/domain/repositories/authentication_repository.dart
@@ -0,0 +1,6 @@
+import '../models/models.dart';
+
+abstract class AuthenticationRepository {
+  Future<bool> get isSignedIn;
+  Future<User> getUserData();
+}
