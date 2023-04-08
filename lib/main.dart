43732cb1d45b168e81c3154afc38c252398bdbee
Oscar Ramirez <osramirezf@gmail.com>
2023-04-07 20:20:41 -0400

    feat: Agregamos clase de Injector el cual extiende de InheritedWidget para el manejo de los repository connectivity & authentication.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'app/data/repository_implementation/authentication_repository_impl.dart';
+import 'app/data/repository_implementation/connectivity_repository_impl.dart';
+import 'app/domain/repositories/repositories.dart';
 import 'app/my_app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(Injector(
+      authenticationRepository: AuthenticationRepositoryImpl(),
+      connectivityRepository: ConnectivityRepositoryImpl(),
+      child: const MyApp()));
+}
+
+class Injector extends InheritedWidget {
+  const Injector({
+    super.key,
+    required super.child,
+    required this.connectivityRepository,
+    required this.authenticationRepository,
+  });
+
+  final ConnectivityRepository connectivityRepository;
+  final AuthenticationRepository authenticationRepository;
+  @override
+  bool updateShouldNotify(_) {
+    throw UnimplementedError();
+  }
+
+  static Injector of(BuildContext context) {
+    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+    assert(injector != null, 'Injector could not be found');
+    return injector!;
+  }
 }
