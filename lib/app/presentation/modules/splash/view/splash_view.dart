6747f125885c5f0733b17e97201b0866b6f26aaa
Oscar Ramirez <osramirezf@gmail.com>
2023-04-07 20:22:20 -0400

    fix: Actualizamos el metodo _init() para que se renderize por por lo menos una vez en el initState para chequear la conectividad a internet.

--- lib/app/presentation/modules/splash/view/splash_view.dart
+++ lib/app/presentation/modules/splash/view/splash_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../main.dart';
+
 class Splash extends StatefulWidget {
   const Splash({super.key});
 
@@ -9,7 +11,28 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _init();
+    });
+  }
+
+  Future<void> _init() async {
+    final connectivityRepository = Injector.of(context).connectivityRepository;
+    final hasInternet = await connectivityRepository.hastInternet;
+
+    if (hasInternet) {
+    } else {}
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+        child: SizedBox(
+            width: 80, height: 80, child: CircularProgressIndicator.adaptive()),
+      ),
+    );
   }
 }
