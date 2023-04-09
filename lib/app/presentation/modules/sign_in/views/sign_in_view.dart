fd267c7e9c959e36055a12cf30bb9eaf7ed5e2d6
Oscar Ramirez <osramirezf@gmail.com>
2023-04-09 16:48:54 -0400

    feat: Agregamos la vista de Sign In

---
+++ lib/app/presentation/modules/sign_in/views/sign_in_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Sign In')),
+    );
+  }
+}
