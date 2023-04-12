73437daba611222757d32659b424c9b7a4a721dc
Oscar Ramirez <osramirezf@gmail.com>
2023-04-11 21:16:32 -0400

    fix: Instanciamos InternetChecker en ConnectivityRepository.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'app/data/repository_implementation/authentication_repository_impl.dart';
 import 'app/data/repository_implementation/connectivity_repository_impl.dart';
+import 'app/data/services/remote/internet_checker.dart';
 import 'app/domain/repositories/repositories.dart';
 import 'app/my_app.dart';
 
 void main() {
   runApp(Injector(
       authenticationRepository: AuthenticationRepositoryImpl(),
-      connectivityRepository: ConnectivityRepositoryImpl(Connectivity()),
+      connectivityRepository: ConnectivityRepositoryImpl(
+        Connectivity(),
+        InternetChecker(),
+      ),
       child: const MyApp()));
 }
 
