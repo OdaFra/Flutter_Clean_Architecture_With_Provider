8ae3bc39d080cd9f2f4a9febdc3ba4ba24444b3c
Oscar Ramirez <osramirezf@gmail.com>
2023-04-11 20:52:28 -0400

    refactor: Actualizamos condicion para evaluar acceso a internet por medio de un metodo

--- lib/app/data/repository_implementation/connectivity_repository_impl.dart
+++ lib/app/data/repository_implementation/connectivity_repository_impl.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import '../../domain/repositories/repositories.dart';
 
 class ConnectivityRepositoryImpl implements ConnectivityRepository {
+  final Connectivity _connectivity;
+
+  ConnectivityRepositoryImpl(this._connectivity);
+
   @override
-  Future<bool> get hastInternet {
-    return Future.value(true);
+  Future<bool> get hastInternet async {
+    final result = await _connectivity.checkConnectivity();
+    if (result == ConnectivityResult.none) {
+      return false;
+    }
+
+    return _hasInternet();
+  }
+
+  Future<bool> _hasInternet() async {
+    try {
+      final list = await InternetAddress.lookup('www.google.com');
+      return (list.isNotEmpty && list.first.rawAddress.isNotEmpty);
+    } catch (e) {
+      return false;
+    }
   }
 }
