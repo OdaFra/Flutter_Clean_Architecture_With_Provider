2b63002d237c35b3ef31a8fe9c0f304aa527fce8
Oscar Ramirez <osramirezf@gmail.com>
2023-04-11 21:15:27 -0400

    feat: Agregamos una clase InternetChecker con su respectiva funcion de hasInternet

---
+++ lib/app/data/services/remote/internet_checker.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+
+class InternetChecker {
+  Future<bool> hasInternet() async {
+    try {
+      //Para comprobar acceso a internet al ejecutar en un navegador
+      if (kIsWeb) {
+        final response = await get(Uri.parse('8.8.8.8'));
+        return response.statusCode == 200;
+      } else {
+        final list = await InternetAddress.lookup('google.com');
+        return (list.isNotEmpty && list.first.rawAddress.isNotEmpty);
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+}
