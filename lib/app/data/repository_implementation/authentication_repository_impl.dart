bf0f774e892a99c03132f11c87f6b6cc4246d42c
Oscar Ramirez <osramirezf@gmail.com>
2023-04-17 07:36:11 -0400

    refactor: Instanciamos el paquete FlutterSecureStorage e implementando en el metodo de isSignedIn.

--- lib/app/data/repository_implementation/authentication_repository_impl.dart
+++ lib/app/data/repository_implementation/authentication_repository_impl.dart
@@ -1,7 +1,15 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../../domain/models/user.dart';
 import '../../domain/repositories/repositories.dart';
 
+const _key = 'sessionId';
+
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
+  final FlutterSecureStorage _secureStorage;
+
+  AuthenticationRepositoryImpl(this._secureStorage);
+
   @override
   Future<User?> getUserData() {
     return Future.value(
@@ -10,7 +18,8 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   }
 
   @override
-  Future<bool> get isSignedIn {
-    return Future.value(false);
+  Future<bool> get isSignedIn async {
+    final sessionId = await _secureStorage.read(key: _key);
+    return sessionId != null;
   }
 }
