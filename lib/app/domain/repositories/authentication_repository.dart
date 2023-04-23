b3995c2aaf40dacc17b379282d8d1092898801a5
Oscar Ramirez <osramirezf@gmail.com>
2023-04-23 19:07:06 -0400

    feat: Agregamos metodo para SignIn manejado los errores por Either.

--- lib/app/domain/repositories/authentication_repository.dart
+++ lib/app/domain/repositories/authentication_repository.dart
@@ -1,6 +1,9 @@
+import '../../core/enums/enum.dart';
+import '../../core/utils/utils.dart';
 import '../models/models.dart';
 
 abstract class AuthenticationRepository {
   Future<bool> get isSignedIn;
   Future<User?> getUserData();
+  Future<Either<SignInFailure, User>> signIn(String username, String password);
 }
