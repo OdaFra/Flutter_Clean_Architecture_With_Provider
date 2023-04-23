9312aa282bd8ec35403e45a2f184e99217dcc30e
Oscar Ramirez <osramirezf@gmail.com>
2023-04-23 17:18:20 -0400

    feat: Agregamos los TextFormField para usuario y pass con sus respectivas validaciones.

--- lib/app/presentation/modules/sign_in/views/sign_in_view.dart
+++ lib/app/presentation/modules/sign_in/views/sign_in_view.dart
@@ -1,12 +1,76 @@
 import 'package:flutter/material.dart';
 
-class SignInView extends StatelessWidget {
+class SignInView extends StatefulWidget {
   const SignInView({super.key});
 
   @override
+  State<SignInView> createState() => _SignInViewState();
+}
+
+class _SignInViewState extends State<SignInView> {
+  String _username = '', _password = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Sign In')),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    onChanged: (value) {
+                      setState(() {
+                        _username = value.trim().toLowerCase();
+                      });
+                    },
+                    validator: (value) {
+                      value = value?.trim().toLowerCase();
+                      if (value!.isEmpty) {
+                        return 'Invalid username';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(hintText: 'username'),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    onChanged: (value) {
+                      setState(() {
+                        _password = value.replaceAll(' ', '').toLowerCase();
+                      });
+                    },
+                    validator: (value) {
+                      value = value?.replaceAll(' ', '').toLowerCase();
+                      if (value!.length < 4) {
+                        return 'Invalid password';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(hintText: 'password'),
+                  ),
+                  const SizedBox(height: 20),
+                  Builder(builder: (context) {
+                    return MaterialButton(
+                      onPressed: () {
+                        final isValid = Form.of(context).validate();
+                        if (isValid) {}
+                      },
+                      color: Colors.indigo,
+                      child: const Text('Sign In'),
+                    );
+                  }),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
