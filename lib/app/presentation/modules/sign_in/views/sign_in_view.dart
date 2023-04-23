4539d634cf88ef2a11f6e7757f1abfda1f18800b
Oscar Ramirez <osramirezf@gmail.com>
2023-04-23 19:45:56 -0400

    refactor: Agregamos funcion _submit con sus condicionales para el manejo del user y pass por los diferentes escenarios agregados en el enum de SignInFailure.

--- lib/app/presentation/modules/sign_in/views/sign_in_view.dart
+++ lib/app/presentation/modules/sign_in/views/sign_in_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../main.dart';
+import '../../../../core/enums/enum.dart';
+import '../../../router/router.dart';
+
 class SignInView extends StatefulWidget {
   const SignInView({super.key});
 
@@ -10,6 +14,8 @@ class SignInView extends StatefulWidget {
 class _SignInViewState extends State<SignInView> {
   String _username = '', _password = '';
 
+  bool _fetching = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,59 +24,104 @@ class _SignInViewState extends State<SignInView> {
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    onChanged: (value) {
-                      setState(() {
-                        _username = value.trim().toLowerCase();
-                      });
-                    },
-                    validator: (value) {
-                      value = value?.trim().toLowerCase();
-                      if (value!.isEmpty) {
-                        return 'Invalid username';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(hintText: 'username'),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    onChanged: (value) {
-                      setState(() {
-                        _password = value.replaceAll(' ', '').toLowerCase();
-                      });
-                    },
-                    validator: (value) {
-                      value = value?.replaceAll(' ', '').toLowerCase();
-                      if (value!.length < 4) {
-                        return 'Invalid password';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(hintText: 'password'),
-                  ),
-                  const SizedBox(height: 20),
-                  Builder(builder: (context) {
-                    return MaterialButton(
-                      onPressed: () {
-                        final isValid = Form.of(context).validate();
-                        if (isValid) {}
+              child: AbsorbPointer(
+                absorbing: _fetching,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      onChanged: (value) {
+                        setState(() {
+                          _username = value.trim().toLowerCase();
+                        });
                       },
-                      color: Colors.indigo,
-                      child: const Text('Sign In'),
-                    );
-                  }),
-                ],
+                      validator: (value) {
+                        value = value?.trim().toLowerCase();
+                        if (value!.isEmpty) {
+                          return 'Invalid username';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(hintText: 'username'),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      onChanged: (value) {
+                        setState(() {
+                          _password = value.replaceAll(' ', '').toLowerCase();
+                        });
+                      },
+                      validator: (value) {
+                        value = value?.replaceAll(' ', '').toLowerCase();
+                        if (value!.length < 4) {
+                          return 'Invalid password';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(hintText: 'password'),
+                    ),
+                    const SizedBox(height: 20),
+                    Builder(builder: (context) {
+                      if (_fetching) {
+                        return const CircularProgressIndicator.adaptive();
+                      }
+                      return MaterialButton(
+                        onPressed: () {
+                          final isValid = Form.of(context).validate();
+                          if (isValid) {
+                            _submit(context);
+                          }
+                        },
+                        color: Colors.indigo,
+                        child: const Text('Sign In'),
+                      );
+                    }),
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> _submit(BuildContext context) async {
+    setState(() {
+      _fetching = true;
+    });
+    final result = await Injector.of(context).authenticationRepository.signIn(
+          _username,
+          _password,
+        );
+    if (!mounted) {
+      return;
+    }
+    result.when(
+      (failure) {
+        setState(() {
+          _fetching = true;
+        });
+        final message = {
+          SignInFailure.notFound: 'Not Found',
+          SignInFailure.unauthorized: 'Invalid password',
+          SignInFailure.unknown: 'Internal error',
+        }[failure];
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(message!),
+          ),
+        );
+      },
+      (user) {
+        Navigator.pushReplacementNamed(
+          context,
+          Routes.home,
+        );
+      },
     );
   }
 }
