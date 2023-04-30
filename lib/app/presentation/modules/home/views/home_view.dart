7ed28503fa3f23e029faddf54daaac6455a27f79
Oscar Ramirez <osramirezf@gmail.com>
2023-04-30 19:22:22 -0400

    feat: Agregamos el metodo para el proceso de Sign Out.

--- lib/app/presentation/modules/home/views/home_view.dart
+++ lib/app/presentation/modules/home/views/home_view.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../main.dart';
+import '../../../presentation.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Home'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Home'),
+            const SizedBox(height: 15),
+            TextButton(
+                onPressed: () async {
+                   Injector.of(context)
+                      .authenticationRepository
+                      .signUout();
+                  Navigator.pushReplacementNamed(context, Routes.signIn);
+                },
+                child: const Text('Sign Out'))
+          ],
+        ),
       ),
     );
   }
