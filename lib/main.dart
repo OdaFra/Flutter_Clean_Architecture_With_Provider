efcada3641476d92ab3df3a4f14bd798571fa236
Oscar Ramirez <osramirezf@gmail.com>
2023-05-06 16:45:14 -0400

    fix: Agregamos instancia para authenticationApi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'app/data/repository_implementation/authentication_repository_impl.dart';
 import 'app/data/repository_implementation/connectivity_repository_impl.dart';
+import 'app/data/services/remote/authentication_api.dart';
 import 'app/data/services/remote/internet_checker.dart';
 import 'app/domain/repositories/repositories.dart';
 import 'app/my_app.dart';
@@ -14,6 +16,9 @@ void main() async {
     Injector(
       authenticationRepository: AuthenticationRepositoryImpl(
         const FlutterSecureStorage(),
+        AuthenticationApi(
+          http.Client(),
+        ),
       ),
       connectivityRepository: ConnectivityRepositoryImpl(
         Connectivity(),
