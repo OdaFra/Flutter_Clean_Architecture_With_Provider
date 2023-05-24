bfe3a9d38f1ab32cb2e1295e4cc2ad8c86138a5f
Oscar Ramirez <osramirezf@gmail.com>
2023-05-23 22:01:58 -0400

    feat: Agregamos funcion para tracking de logs como stackTrace

---
+++ lib/app/data/http/logs.dart
@@ -0,0 +1,20 @@
+part of 'HttpManagement.dart';
+
+_printLogs(
+  Map<String, dynamic> logs,
+  StackTrace? stackTrace,
+) {
+  var spaces = ' ' * 4;
+  var encoder = JsonEncoder.withIndent(spaces);
+  if (kDebugMode) {
+    log(
+      '''
+Estos son los logs
+-----------------------------------------------------------
+${encoder.convert(logs).toString()}
+-----------------------------------------------------------
+''',
+      stackTrace: stackTrace,
+    );
+  }
+}
