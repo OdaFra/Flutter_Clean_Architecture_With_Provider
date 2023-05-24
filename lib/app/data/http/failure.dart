4c9fccb458c8034a729f5f74e25744a9c6af8edd
Oscar Ramirez <osramirezf@gmail.com>
2023-05-23 22:00:31 -0400

    feat: Agregamos archivo para la clase de HttoFailure

---
+++ lib/app/data/http/failure.dart
@@ -0,0 +1,13 @@
+part of 'HttpManagement.dart';
+
+class HttpFailure {
+  HttpFailure({
+    this.statusCode,
+    this.exception,
+  });
+
+  final int? statusCode;
+  final Object? exception;
+}
+
+class NetworKException {}
