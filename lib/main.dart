fbb874450a97dff62e6558221ff130eb63a9fbcb
Oscar Ramirez <osramirezf@gmail.com>
2023-05-28 15:31:06 -0400

    refactor: Eliminamos la instancia del Injector para utilizar el paquete de provider.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
 import 'app/data/http/httpManagement.dart';
 import 'app/data/repository_implementation/authentication_repository_impl.dart';
 import 'app/data/repository_implementation/connectivity_repository_impl.dart';
@@ -15,44 +16,44 @@ void main() async {
   await dotenv.load();
 
   runApp(
-    Injector(
-      authenticationRepository: AuthenticationRepositoryImpl(
-        const FlutterSecureStorage(),
-        AuthenticationApi(
-          HttpManagement(
-            client: http.Client(),
-            baseUrl: dotenv.env['BASE_URL']!,
-            apiKey: dotenv.env['TMDB_KEY']!,
+    Provider<ConnectivityRepository>(
+      create: (context) => ConnectivityRepositoryImpl(
+        connectivity: Connectivity(),
+        internetChecker: InternetChecker(),
+      ),
+      child: Provider<AuthenticationRepository>(
+        create: (context) => AuthenticationRepositoryImpl(
+          const FlutterSecureStorage(),
+          AuthenticationApi(
+            HttpManagement(
+              client: http.Client(),
+              baseUrl: dotenv.env['BASE_URL']!,
+              apiKey: dotenv.env['TMDB_KEY']!,
+            ),
           ),
         ),
+        child: const MyApp(),
       ),
-      connectivityRepository: ConnectivityRepositoryImpl(
-        Connectivity(),
-        InternetChecker(),
-      ),
-      child: const MyApp(),
     ),
   );
 }
 
-class Injector extends InheritedWidget {
-  const Injector({
-    super.key,
-    required super.child,
-    required this.connectivityRepository,
-    required this.authenticationRepository,
-  });
+// class Injector extends InheritedWidget {
+//   const Injector({
+//     super.key,
+//     required super.child,
+//     required this.authenticationRepository,
+//   });
 
-  final ConnectivityRepository connectivityRepository;
-  final AuthenticationRepository authenticationRepository;
-  @override
-  bool updateShouldNotify(_) {
-    throw UnimplementedError();
-  }
+//   final AuthenticationRepository authenticationRepository;
+//   @override
+//   bool updateShouldNotify(_) {
+//     throw UnimplementedError();
+//   }
 
-  static Injector of(BuildContext context) {
-    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
-    assert(injector != null, 'Injector could not be found');
-    return injector!;
-  }
-}
+//   static Injector of(BuildContext context) {
+//     final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+//     assert(injector != null, 'Injector could not be found');
+//     return injector!;
+//   }
+// }
