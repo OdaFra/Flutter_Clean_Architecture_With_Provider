dbdfe050851e95f7726462db96bb7ffd9913bc85
Oscar Ramirez <osramirezf@gmail.com>
2023-06-06 22:53:16 -0400

    feat: Agregamos la funcion de dispone como una variable bool para el manejo de mounted

--- lib/app/presentation/modules/sign_in/controllers/signIn_controller.dart
+++ lib/app/presentation/modules/sign_in/controllers/signIn_controller.dart
@@ -3,11 +3,12 @@ import 'package:flutter/foundation.dart';
 class SigInController extends ChangeNotifier {
   String _username = '';
   String _password = '';
-  bool _fetching = false;
+  bool _fetching = false, _mounted = true;
 
   String get username => _username;
   String get password => _password;
   bool get fetching => _fetching;
+  bool get mounted => _mounted;
 
   void onUsernameChanged(String text) {
     _username = text.trim().toLowerCase();
@@ -20,5 +21,11 @@ class SigInController extends ChangeNotifier {
   void onFetchingChanged(bool value) {
     _fetching = value;
     notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _mounted = !mounted;
+    super.dispose();
   }
 }
