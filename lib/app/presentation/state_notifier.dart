794ac659593c9dd855ee8cbe5733cfdc04289603
Oscar Ramirez <osramirezf@gmail.com>
2023-06-11 16:29:43 -0400

    feat: Agregamos metodos para manejar la actualizacion del estado como mantener el estado anterior.

--- lib/app/presentation/state_notifier.dart
+++ lib/app/presentation/state_notifier.dart
@@ -1,3 +1,38 @@
 import 'package:flutter/foundation.dart';
 
-abstract class StateNotifier extends ChangeNotifier {}
+abstract class StateNotifier<State> extends ChangeNotifier {
+  StateNotifier(this._state) : _oldstate = _state;
+  late State _state, _oldstate;
+  bool _mounted = true;
+
+  State get state => _state;
+  State get oldstate => _oldstate;
+  bool get mounted => _mounted;
+
+  set state(State newState) {
+    _update(newState);
+  }
+
+  void onlyUpdate(State newState) {
+    _update(newState, notify: false);
+  }
+
+  void _update(
+    State newState, {
+    bool notify = true,
+  }) {
+    if (_state != newState) {
+      _oldstate = _state;
+      _state = newState;
+      if (notify) {
+        notifyListeners();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _mounted = !mounted;
+    super.dispose();
+  }
+}
