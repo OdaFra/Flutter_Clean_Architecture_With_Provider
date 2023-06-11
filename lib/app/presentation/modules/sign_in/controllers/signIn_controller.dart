365142e039d880b93b7044e8e22297ae6a1169fc
Oscar Ramirez <osramirezf@gmail.com>
2023-06-11 16:31:05 -0400

    refactor: Actualizamos los metodos en base la clase de StateNotifier.

--- lib/app/presentation/modules/sign_in/controllers/signIn_controller.dart
+++ lib/app/presentation/modules/sign_in/controllers/signIn_controller.dart
@@ -1,39 +1,27 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import '../../../state_notifier.dart';
 import 'signIn_state.dart';
-import 'package:flutter/foundation.dart';
 
-class SigInController extends ChangeNotifier {
-  SignInState _state = const SignInState();
-
-  SignInState get state => _state;
-
-  bool _mounted = true;
-
-  bool get mounted => _mounted;
+class SigInController extends StateNotifier<SignInState> {
+  SigInController(super.state);
 
   void onUsernameChanged(String text) {
-    _state = _state.copyWith(
-      username: text.trim().toLowerCase(),
+    onlyUpdate(
+      state.copyWith(
+        username: text.trim().toLowerCase(),
+      ),
     );
-    notifyListeners();
   }
 
   void onPasswordChanged(String text) {
-    _state = _state.copyWith(
-      password: text.replaceAll(' ', ''),
+    onlyUpdate(
+      state.copyWith(
+        password: text.replaceAll(' ', ''),
+      ),
     );
-    notifyListeners();
   }
 
   void onFetchingChanged(bool value) {
-    _state = _state.copyWith(fetching: value);
-
-    notifyListeners();
-  }
-
-  @override
-  void dispose() {
-    _mounted = !mounted;
-    super.dispose();
+    state = state.copyWith(fetching: value);
   }
 }
