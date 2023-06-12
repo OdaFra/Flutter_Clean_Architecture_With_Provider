7b5ef8130acb1e1ac1daacf42b0a80414e5e69eb
Oscar Ramirez <osramirezf@gmail.com>
2023-06-12 19:17:02 -0400

    feat: Implementamos AccountRepository para utitlizar la funcion de getUserData con la finalidad de obtener informacion del usuario.

--- lib/app/data/repository_implementation/account_repository_impl.dart
+++ lib/app/data/repository_implementation/account_repository_impl.dart
@@ -1,3 +1,20 @@
+import '../../domain/models/user.dart';
 import '../../domain/repositories/account_repository.dart';
+import '../services/local/session_service.dart';
+import '../services/remote/remote.dart';
 
-class AccountRepositoryImpl implements AccountRepository {}
+class AccountRepositoryImpl implements AccountRepository {
+  AccountRepositoryImpl(
+    this._accountApi,
+    this._sessionService,
+  );
+
+  final AccountApi _accountApi;
+  final SessionService _sessionService;
+  @override
+  Future<User?> getUserData() async {
+    return _accountApi.getAccount(
+      await _sessionService.sessionId ?? '',
+    );
+  }
+}
