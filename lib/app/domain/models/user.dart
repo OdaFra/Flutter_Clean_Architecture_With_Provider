186853e61cc035f9f86037445397a1c9bfc2e0dc
Oscar Ramirez <osramirezf@gmail.com>
2023-06-13 10:09:57 -0400

    refactor: Refactorizamos clase User agregando JSON serializable para obtener el fromMap como toMap.

--- lib/app/domain/models/user.dart
+++ lib/app/domain/models/user.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class User {
   User({
     required this.id,
@@ -6,4 +9,23 @@ class User {
 
   final int id;
   final String username;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'username': username,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as int,
+      username: map['username'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
 }
