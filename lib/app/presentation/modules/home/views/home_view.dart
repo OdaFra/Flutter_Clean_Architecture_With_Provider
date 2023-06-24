9df65f8905609813226096419421c81e5ae3a1df
Oscar Ramirez <osramirezf@gmail.com>
2023-06-24 12:35:10 -0400

    refactor: Aplicamos cambios de StatelessWidget a StatefulWidget como tambien agregando condicional para el manejo de SignOut y navegacion a la vista de SignIn

--- lib/app/presentation/modules/home/views/home_view.dart
+++ lib/app/presentation/modules/home/views/home_view.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../../../domain/repositories/repositories.dart';
 import '../../../global/controllers/session_controller.dart';
 import '../../../presentation.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
-    final sessionController = context.read<SessionController>();
+    final SessionController sessionController =
+        Provider.of(context); //context.read<SessionController>();
+    final user = sessionController.state!;
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('Home'),
-            Text('${sessionController.state!.id}'),
-            Text(sessionController.state!.username ?? ''),
+            Text('${user.id}'),
+            Text(user.username ?? ''),
             const SizedBox(height: 15),
             TextButton(
                 onPressed: () async {
-                  context.read<AuthenticationRepository>().signOut();
-                  Navigator.pushReplacementNamed(context, Routes.signIn);
+                  await sessionController.signOut();
+                  if (mounted) {
+                    Navigator.pushReplacementNamed(context, Routes.signIn);
+                  }
                 },
                 child: const Text('Sign Out'))
           ],
