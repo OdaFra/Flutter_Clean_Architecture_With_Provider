7c3191fd69a6a586629b93e84e2db9dbab3ef604
Oscar Ramirez <osramirezf@gmail.com>
2023-07-21 15:36:42 -0400

    feat: Generamos clase abstracta para el manejo de los diferentes Failure a presentarse

---
+++ lib/app/domain/failures/sign_in_failure.dart
@@ -0,0 +1,9 @@
+abstract class SignInFailure {}
+
+class Network extends SignInFailure {}
+
+class NotFound extends SignInFailure {}
+
+class Unauthorized extends SignInFailure {}
+
+class Unkonwn extends SignInFailure {}
