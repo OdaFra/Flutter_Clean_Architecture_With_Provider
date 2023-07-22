b4954035e6313f7e491656d38a83d828a68ada41
Oscar Ramirez <osramirezf@gmail.com>
2023-07-22 15:52:28 -0400

    refactor: Implementamos la libreria freezed para el manejo de los either.

--- lib/app/core/utils/either.dart
+++ lib/app/core/utils/either.dart
@@ -1,25 +1,32 @@
-class Either<Left, Right> {
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Either._(this._left, this._right, this.isLeft);
+part 'either.freezed.dart';
 
-  factory Either.left(Left failure) {
-    return Either._(failure, null, true);
-  }
-  factory Either.right(Right value) {
-    return Either._(null, value, false);
-  }
-  final Left? _left;
-  final Right? _right;
-  final bool isLeft;
-
-  T when<T>(
-    T Function(Left) left,
-    T Function(Right) right,
-  ) {
-    if (isLeft) {
-      return left(_left as Left);
-    } else {
-      return right(_right as Right);
-    }
-  }
+@freezed
+class Either<L, R> with _$Either<L, R> {
+  factory Either.left(L value) = Left;
+  factory Either.right(R value) = Right;
 }
+
+ //  Either._(this._left, this._right, this.isLeft);
+
+  // factory Either.left(Left failure) {
+  //   return Either._(failure, null, true);
+  // }
+  // factory Either.right(Right value) {
+  //   return Either._(null, value, false);
+  // }
+  // final Left? _left;
+  // final Right? _right;
+  // final bool isLeft;
+
+  // T when<T>(
+  //   T Function(Left) left,
+  //   T Function(Right) right,
+  // ) {
+  //   if (isLeft) {
+  //     return left(_left as Left);
+  //   } else {
+  //     return right(_right as Right);
+  //   }
+  // }
