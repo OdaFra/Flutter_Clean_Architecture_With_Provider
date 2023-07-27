ac9638f1197c623afa3f5a513573fa4b1f91051e
Oscar Ramirez <osramirezf@gmail.com>
2023-07-27 18:36:28 -0400

    refactor: Agregamos un nuevo atributo de tipo object para el manejo de json.

--- lib/app/data/http/failure.dart
+++ lib/app/data/http/failure.dart
@@ -2,10 +2,12 @@ class HttpFailure {
   HttpFailure({
     this.statusCode,
     this.exception,
+    this.data,
   });
 
   final int? statusCode;
   final Object? exception;
+  final Object? data;
 }
 
 class NetworKException {}
