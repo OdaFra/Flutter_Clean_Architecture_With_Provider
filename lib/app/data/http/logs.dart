55f124bedd30e67a4d4c203dcfd471929d828019
Oscar Ramirez <osramirezf@gmail.com>
2023-08-06 17:05:03 -0400

    refactor: Agregamos trycast para el manejo de errores.

--- lib/app/data/http/logs.dart
+++ lib/app/data/http/logs.dart
@@ -7,17 +7,21 @@ printLogs(
   Map<String, dynamic> logs,
   StackTrace? stackTrace,
 ) {
-  var spaces = ' ' * 4;
-  var encoder = JsonEncoder.withIndent(spaces);
-  if (kDebugMode) {
-    log(
-      '''
+  try {
+    var spaces = ' ' * 4;
+    var encoder = JsonEncoder.withIndent(spaces);
+    if (kDebugMode) {
+      log(
+        '''
 Estos son los logs
 -----------------------------------------------------------
 ${encoder.convert(logs).toString()}
 -----------------------------------------------------------
 ''',
-      stackTrace: stackTrace,
-    );
+        stackTrace: stackTrace,
+      );
+    }
+  } catch (e) {
+    log(e.toString());
   }
 }
