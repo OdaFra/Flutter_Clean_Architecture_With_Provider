267b9bd00b17304f34b80897914e703a658d263f
Oscar Ramirez <osramirezf@gmail.com>
2023-08-07 21:25:37 -0400

    refactor: Agregamos el widget TrendingList el cual obtiene la lista por FutureBuilder

--- lib/app/presentation/modules/home/views/home_view.dart
+++ lib/app/presentation/modules/home/views/home_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../global/controllers/session_controller.dart';
-import '../../../presentation.dart';
+import '../widgets/widgets.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,27 +18,29 @@ class _HomeViewState extends State<HomeView> {
         Provider.of(context); //context.read<SessionController>();
     final user = sessionController.state!;
 
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (user.avatarPath != null)
-              Image.network(
-                  'https://image.tmdb.org/t/p/w500${user.avatarPath}'),
-            const SizedBox(height: 15),
-            const Text('Home'),
-            Text('${user.id}'),
-            Text(user.username),
-            const SizedBox(height: 15),
-            TextButton(
-                onPressed: () async {
-                  await sessionController.signOut();
-                  if (mounted) {
-                    Navigator.pushReplacementNamed(context, Routes.signIn);
-                  }
-                },
-                child: const Text('Sign Out'))
+            TrendingList()
+
+            // if (user.avatarPath != null)
+            //   Image.network(
+            //       'https://image.tmdb.org/t/p/w500${user.avatarPath}'),
+            // const SizedBox(height: 15),
+            // const Text('Home'),
+            // Text('${user.id}'),
+            // Text(user.username),
+            // const SizedBox(height: 15),
+            // TextButton(
+            //     onPressed: () async {
+            //       await sessionController.signOut();
+            //       if (mounted) {
+            //         Navigator.pushReplacementNamed(context, Routes.signIn);
+            //       }
+            //     },
+            //     child: const Text('Sign Out'))
           ],
         ),
       ),
