74252c4666f11cc82562311c5c65d7c825161e23
Oscar Ramirez <osramirezf@gmail.com>
2023-08-13 15:23:30 -0400

    feat: Agregamos dos funciones para title y original_title, ya que pueden variar el nombre de cada uno de los parametros.

--- lib/app/domain/models/media/media.dart
+++ lib/app/domain/models/media/media.dart
@@ -9,9 +9,10 @@ part 'media.g.dart';
 class Media with _$Media {
   factory Media({
     required int id,
-    required String title,
     required String overview,
-    @JsonKey(name: 'original_title') required String originalTitle,
+    @JsonKey(readValue: readTitleValue) required String title,
+    @JsonKey(name: 'original_title', readValue: readOriginalValue)
+    required String originalTitle,
     @JsonKey(name: 'poster_path') required String posterPath,
     @JsonKey(name: 'backdrop_path') required String backdropPath,
     @JsonKey(name: 'vote_average') required double voteAverage,
@@ -19,4 +20,12 @@ class Media with _$Media {
   }) = _Media;
 
   factory Media.fromJson(Json json) => _$MediaFromJson(json);
+}
+
+Object? readTitleValue(Map map, String _) {
+  return map['title'] ?? map['name'];
+}
+
+Object? readOriginalValue(Map map, String _) {
+  return map['original_title'] ?? map['original_name'];
 }
