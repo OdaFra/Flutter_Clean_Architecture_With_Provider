6fffa07f2df753e10886c10b11c895d15074b829
Oscar Ramirez <osramirezf@gmail.com>
2023-09-16 16:18:33 -0400

    feat: Agregamos funcion getPerformers para obtener lista de actores.

--- lib/app/domain/repositories/trending_repository.dart
+++ lib/app/domain/repositories/trending_repository.dart
@@ -2,9 +2,11 @@ import '../../core/enums/enum.dart';
 import '../../core/utils/utils.dart';
 import '../failures/http_request/http_request_failure.dart';
 import '../models/media/media.dart';
+import '../models/peformer/performer.dart';
 
 abstract class TrendingRepository {
   Future<Either<HttpRequestFailure, List<Media>>> getMoviesAndSeries(
     TimeWindow timeWindow,
   );
+  Future<Either<HttpRequestFailure, List<Performer>>> getPerformers();
 }
