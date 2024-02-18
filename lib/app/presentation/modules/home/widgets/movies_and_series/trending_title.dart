c70e96a3b8c31145ab10a22c910d8bfb44722239
Oscar Ramirez <osramirezf@gmail.com>
2024-02-18 15:55:34 -0300

    feat: Agregamos showdata=true con la finalidad de agregar visualizacion segun se necesite en el widget TrendingTitle

--- lib/app/presentation/modules/home/widgets/movies_and_series/trending_title.dart
+++ lib/app/presentation/modules/home/widgets/movies_and_series/trending_title.dart
@@ -5,21 +5,25 @@ import '../../../../../domain/models/media/media.dart';
 import '../../../../global/global.dart';
 
 class TrendingTitle extends StatelessWidget {
-  const TrendingTitle({super.key, required this.media, required this.width});
+  const TrendingTitle(
+      {super.key,
+      required this.media,
+      required this.width,
+      this.showData = true});
   final Media media;
   final double width;
+  final bool showData;
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
+      borderRadius: BorderRadius.circular(12),
       child: SizedBox(
         width: width,
         child: Stack(
           children: [
             Positioned.fill(
               child: ExtendedImage.network(
-                
                 getImageUrl(media.posterPath),
                 fit: BoxFit.cover,
                 loadStateChanged: (state) {
@@ -32,37 +36,44 @@ class TrendingTitle extends StatelessWidget {
                 },
               ),
             ),
-            Positioned(
-                top: 5,
-                right: 5,
-                child: Opacity(
-                  opacity: 0.7,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Chip(
-                        backgroundColor: Colors.grey.shade100,
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        label: Text(
-                          media.voteAverage.toStringAsFixed(1),
-                          style: TextStyle(
-                              color: Colors.blueGrey.shade600, fontSize: 12),
+            if (showData)
+              Positioned(
+                  top: 5,
+                  right: 5,
+                  child: Opacity(
+                    opacity: 0.85,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Chip(
+                          backgroundColor: Colors.grey.shade100,
+                          labelPadding: const EdgeInsets.all(0.5),
+                          materialTapTargetSize:
+                              MaterialTapTargetSize.shrinkWrap,
+                          label: Text(
+                            media.voteAverage.toStringAsFixed(1),
+                            style: TextStyle(
+                                color: Colors.blueGrey.shade600,
+                                fontSize: 10,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 4),
-                      Chip(
-                        backgroundColor: Colors.grey.shade100,
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        label: Icon(
-                          media.type == MediaType.movie
-                              ? Icons.movie
-                              : Icons.tv,
-                          size: 15,
+                        const SizedBox(height: 4),
+                        Chip(
+                          labelPadding: const EdgeInsets.all(0.5),
+                          backgroundColor: Colors.grey.shade100,
+                          materialTapTargetSize:
+                              MaterialTapTargetSize.shrinkWrap,
+                          label: Icon(
+                            media.type == MediaType.movie
+                                ? Icons.movie
+                                : Icons.tv,
+                            size: 16,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                )),
+                      ],
+                    ),
+                  )),
           ],
         ),
       ),
