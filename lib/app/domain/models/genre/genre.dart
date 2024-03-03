852b9dd25295e6bd2aab6b020f0679c6db5ec117
Oscar Ramirez <osramirezf@gmail.com>
2024-03-03 16:46:55 -0300

    models: Se genera modelos de genero y detalles de peliculas.

---
+++ lib/app/domain/models/genre/genre.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../core/utils/typedefs.dart';
+
+part 'genre.freezed.dart';
+part 'genre.g.dart';
+
+@freezed
+class Genre with _$Genre {
+  factory Genre({required int id, required String name}) = _Genre;
+
+  factory Genre.fromJson(Json json) => _$GenreFromJson(json);
+}
---
+++ lib/app/domain/models/movies/movies.dart
@@ -0,0 +1,26 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../core/utils/typedefs.dart';
+import '../genre/genre.dart';
+import '../media/media.dart';
+
+part 'movies.freezed.dart';
+part 'movies.g.dart';
+
+@freezed
+class Movie with _$Movie {
+  factory Movie({
+    required int id,
+    required List<Genre> genres,
+    required String overview,
+    required int runtime,
+    @JsonKey(name: 'poster_path') required String posterPath,
+    @JsonKey(name: 'release_date') required DateTime releaseDate,
+    @JsonKey(name: 'vote_average') required double voteAverage,
+    @JsonKey(readValue: readTitleValue) required String title,
+    @JsonKey(readValue: readOriginalValue) required String originalTitle,
+    @JsonKey(name: 'backdrop_path') required String backdropPath,
+  }) = _Movie;
+
+  factory Movie.fromJson(Json json) => _$MovieFromJson(json);
+}
