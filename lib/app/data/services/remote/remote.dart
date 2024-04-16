c1e8c24a0ed49dfab0a8d07af509a55c4cea3e12
Oscar Ramirez <osramirezf@gmail.com>
2024-04-16 15:47:09 -0400

    fix: Actualizamos archivos de barril

--- lib/app/data/services/remote/remote.dart
+++ lib/app/data/services/remote/remote.dart
@@ -1,3 +1,5 @@
 export 'account_api.dart';
 export 'authentication_api.dart';
 export 'internet_checker.dart';
+export 'movie_api.dart';
+export 'trending_api.dart';
--- lib/app/domain/repositories/repositories.dart
+++ lib/app/domain/repositories/repositories.dart
@@ -1,4 +1,5 @@
 export 'account_repository.dart';
 export 'authentication_repository.dart';
 export 'connectivity_repository.dart';
+export 'movie_repository.dart';
 export 'trending_repository.dart';
