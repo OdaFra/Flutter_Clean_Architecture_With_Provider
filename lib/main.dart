eda1e740a693e76600da5d939848df94b6aefb4a
Oscar Ramirez <osramirezf@gmail.com>
2024-04-16 15:48:33 -0400

    feature: Agregamos movie state, controller e instanciamos en main para disponibilizar en el arbol del widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,13 @@ import 'app/data/http/httpManagement.dart';
 import 'app/data/repository_implementation/account_repository_impl.dart';
 import 'app/data/repository_implementation/authentication_repository_impl.dart';
 import 'app/data/repository_implementation/connectivity_repository_impl.dart';
+import 'app/data/repository_implementation/movie_repository_impl.dart';
 import 'app/data/repository_implementation/trending_repository_impl.dart';
 import 'app/data/services/local/session_service.dart';
 import 'app/data/services/remote/account_api.dart';
 import 'app/data/services/remote/authentication_api.dart';
 import 'app/data/services/remote/internet_checker.dart';
+import 'app/data/services/remote/movie_api.dart';
 import 'app/data/services/remote/trending_api.dart';
 import 'app/domain/repositories/repositories.dart';
 import 'app/my_app.dart';
@@ -40,21 +42,17 @@ void main() async {
         ),
         Provider<ConnectivityRepository>(
           create: (context) => ConnectivityRepositoryImpl(
-            connectivity: Connectivity(),
-            internetChecker: InternetChecker(),
-          ),
+              connectivity: Connectivity(), internetChecker: InternetChecker()),
         ),
         Provider<AuthenticationRepository>(
           create: (context) => AuthenticationRepositoryImpl(
-            AuthenticationApi(http),
-            sessionService,
-            accountApi,
-          ),
+              AuthenticationApi(http), sessionService, accountApi),
         ),
         Provider<TrendingRepository>(
-          create: (_) => TrendingRepositoryImpl(
-            TrendingAPI(http),
-          ),
+          create: (_) => TrendingRepositoryImpl(TrendingAPI(http)),
+        ),
+        Provider<MovieRepository>(
+          create: (_) => MovieRepositoryImpl(MovieApi(http)),
         ),
         ChangeNotifierProvider<SessionController>(
           create: (context) => SessionController(
