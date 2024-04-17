27a174c687b247ca9d0aad3b570303e28fad8774
Oscar Ramirez <osramirezf@gmail.com>
2024-04-17 18:13:14 -0400

    feature: Agregamos Inkwell para el OnTap de navegacion hacia la vista MovieView

--- lib/app/presentation/modules/home/widgets/movies_and_series/trending_title.dart
+++ lib/app/presentation/modules/home/widgets/movies_and_series/trending_title.dart
@@ -16,65 +16,72 @@ class TrendingTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: SizedBox(
-        width: width,
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: ExtendedImage.network(
-                getImageUrl(media.posterPath),
-                fit: BoxFit.cover,
-                loadStateChanged: (state) {
-                  if (state.extendedImageLoadState == LoadState.loading) {
-                    return Container(
-                      color: Colors.black12,
-                    );
-                  }
-                  return state.completedWidget;
-                },
+    return InkWell(
+      onTap: () {
+        if (media.type == MediaType.movie) {
+          Navigator.pushNamed(context, 'movie', arguments: media.id);
+        }
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: SizedBox(
+          width: width,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: ExtendedImage.network(
+                  getImageUrl(media.posterPath),
+                  fit: BoxFit.cover,
+                  loadStateChanged: (state) {
+                    if (state.extendedImageLoadState == LoadState.loading) {
+                      return Container(
+                        color: Colors.black12,
+                      );
+                    }
+                    return state.completedWidget;
+                  },
+                ),
               ),
-            ),
-            if (showData)
-              Positioned(
-                  top: 5,
-                  right: 5,
-                  child: Opacity(
-                    opacity: 0.85,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Chip(
-                          backgroundColor: Colors.grey.shade100,
-                          labelPadding: const EdgeInsets.all(0.5),
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          label: Text(
-                            media.voteAverage.toStringAsFixed(1),
-                            style: TextStyle(
-                                color: Colors.blueGrey.shade600,
-                                fontSize: 10,
-                                fontWeight: FontWeight.bold),
+              if (showData)
+                Positioned(
+                    top: 5,
+                    right: 5,
+                    child: Opacity(
+                      opacity: 0.85,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Chip(
+                            backgroundColor: Colors.grey.shade100,
+                            labelPadding: const EdgeInsets.all(0.5),
+                            materialTapTargetSize:
+                                MaterialTapTargetSize.shrinkWrap,
+                            label: Text(
+                              media.voteAverage.toStringAsFixed(1),
+                              style: TextStyle(
+                                  color: Colors.blueGrey.shade600,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 4),
-                        Chip(
-                          labelPadding: const EdgeInsets.all(0.5),
-                          backgroundColor: Colors.grey.shade100,
-                          materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                          label: Icon(
-                            media.type == MediaType.movie
-                                ? Icons.movie
-                                : Icons.tv,
-                            size: 16,
+                          const SizedBox(height: 4),
+                          Chip(
+                            labelPadding: const EdgeInsets.all(0.5),
+                            backgroundColor: Colors.grey.shade100,
+                            materialTapTargetSize:
+                                MaterialTapTargetSize.shrinkWrap,
+                            label: Icon(
+                              media.type == MediaType.movie
+                                  ? Icons.movie
+                                  : Icons.tv,
+                              size: 16,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )),
-          ],
+                        ],
+                      ),
+                    )),
+            ],
+          ),
         ),
       ),
     );
