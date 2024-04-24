bd0642f4870235e5d7389d1b8d0b50016a980f6f
Oscar Ramirez <osramirezf@gmail.com>
2024-04-24 19:19:44 -0400

    fix: Mantenemos estructura original y eliminamos la propiedades de onGenerateRoute

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'presentation/modules/home/views/home_view.dart';
-import 'presentation/modules/movie/views/movie_view.dart';
 import 'presentation/router/router.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,28 +9,10 @@ class MyApp extends StatelessWidget {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute: Routes.splash,
-          routes: appRoutes,
-          onGenerateRoute: (settings) {
-            final name = settings.name ?? '';
-            if (name == Routes.movie) {
-              return MaterialPageRoute(
-                  settings: const RouteSettings(name: Routes.home),
-                  builder: (_) => const HomeView());
-            }
-
-            if (name.startsWith(Routes.movie)) {
-              final id = int.parse(
-                name.substring(name.lastIndexOf('/') + 1, name.length),
-              );
-              return MaterialPageRoute(
-                builder: (_) => MovieView(movieId: id),
-                settings: settings,
-              );
-            }
-            return null;
-          }),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.splash,
+        routes: appRoutes,
+      ),
     );
   }
 }
