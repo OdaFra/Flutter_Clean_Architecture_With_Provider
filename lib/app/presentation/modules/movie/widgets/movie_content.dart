1defd10c4dd1bbc882b6aefb41dbf0c4ed7d686c
Oscar Ramirez <osramirezf@gmail.com>
2024-05-11 18:34:36 -0400

    refactor: Aplicamos refactor para separar en un widget idependiente el header del movie.

--- lib/app/presentation/modules/movie/widgets/movie_content.dart
+++ lib/app/presentation/modules/movie/widgets/movie_content.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../global/global.dart';
 import '../controllers/state/movie_state.dart';
+import 'movie_header.dart';
 
 class MovieContent extends StatelessWidget {
   const MovieContent({super.key, required this.state});
@@ -14,76 +15,16 @@ class MovieContent extends StatelessWidget {
     return SingleChildScrollView(
         child: Column(
       children: [
-        Stack(
-          children: [
-            AspectRatio(
-              aspectRatio: 16 / 14,
-              child: ExtendedImage.network(
-                getImageUrl(movie.backdropPath,
-                    imageQuality: ImageQuality.original),
-                width: double.infinity,
-                height: double.infinity,
-                fit: BoxFit.cover,
+        MovieHeader(movie: movie),
+        Padding(
+            padding: const EdgeInsets.all(10),
+            child: Text(
+              movie.overview,
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Colors.black54,
-                        Colors.black
-                      ]),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 15).copyWith(
-                  top: 25,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      movie.title,
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    const SizedBox(height: 10),
-                    Wrap(
-                      spacing: 10,
-                      children: movie.genres
-                          .map((genre) => Container(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 5),
-                                decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: Colors.white,
-                                        width: 2,
-                                        style: BorderStyle.solid),
-                                    borderRadius: BorderRadius.circular(5)),
-                                child: Text(
-                                  genre.name,
-                                  style: const TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w800,
-                                      color: Colors.white),
-                                ),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: 12)
-                  ],
-                ),
-              ),
-            ),
-          ],
-        )
+            )),
       ],
     ));
   }
---
+++ lib/app/presentation/modules/movie/widgets/movie_header.dart
@@ -0,0 +1,113 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../domain/models/movies/movies.dart';
+import '../../../global/global.dart';
+
+class MovieHeader extends StatelessWidget {
+  const MovieHeader({super.key, required this.movie});
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: 16 / 14,
+          child: ExtendedImage.network(
+            getImageUrl(movie.backdropPath,
+                imageQuality: ImageQuality.original),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.transparent, Colors.black54, Colors.black]),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15).copyWith(
+              top: 25,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: const TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(height: 10),
+                      Wrap(
+                        spacing: 10,
+                        children: movie.genres
+                            .map((genre) => Container(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 5),
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.white,
+                                          width: 2,
+                                          style: BorderStyle.solid),
+                                      borderRadius: BorderRadius.circular(5)),
+                                  child: Text(
+                                    genre.name,
+                                    style: const TextStyle(
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w800,
+                                        color: Colors.white),
+                                  ),
+                                ))
+                            .toList(),
+                      ),
+                      const SizedBox(height: 12)
+                    ],
+                  ),
+                ),
+                Column(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                            strokeWidth: 2,
+                            value: (movie.voteAverage / 10).clamp(0.0, 1.0),
+                          ),
+                        ),
+                        Text(
+                          movie.voteAverage.toStringAsPrecision(2),
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 12)
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
