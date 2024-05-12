f6bff2141446f235301d97d5aff0adf48f6e3ba8
Oscar Ramirez <osramirezf@gmail.com>
2024-05-12 12:21:16 -0400

    feature: Agregamos funcion getFavorites para obtener la lista de favoritos(Movies/TV)

--- lib/app/data/services/remote/account_api.dart
+++ lib/app/data/services/remote/account_api.dart
@@ -1,9 +1,16 @@
+import '../../../core/utils/either.dart';
+import '../../../domain/failures/http_request/http_request_failure.dart';
+import '../../../domain/models/media/media.dart';
 import '../../../domain/models/models.dart';
+import '../../http/http.dart';
 import '../../http/httpManagement.dart';
+import '../local/session_service.dart';
+import '../utils/handle_failure.dart';
 
 class AccountApi {
-  AccountApi(this._httpManagement);
+  AccountApi(this._httpManagement, this._sessionService);
   final HttpManagement _httpManagement;
+  final SessionService _sessionService;
 
   Future<User?> getAccount(String sessionId) async {
     final result = await _httpManagement.request(
@@ -19,6 +26,30 @@ class AccountApi {
     return result.when(
       left: (_) => null,
       right: (user) => user,
+    );
+  }
+
+  Future<Either<HttpRequestFailure, Map<int, Media>>> getFavorites(
+      MediaType type) async {
+    final sessionId = await _sessionService.sessionId ?? '';
+    final accountId = await _sessionService.accountId;
+
+    final result = await _httpManagement.request(
+        '/account/$accountId/favorite/${type == MediaType.movie ? 'movies' : 'tv'}',
+        queryParameters: {'session_id': sessionId}, onSuccess: (json) {
+      final list = json['results'] as List;
+      final iterableList = list.map((e) {
+        final media = Media.fromJson(json);
+        return MapEntry(media.id, media);
+      });
+      final map = <int, Media>{};
+      map.addEntries(iterableList);
+      return map;
+    });
+
+    return result.when(
+      left: handleHttpFailure,
+      right: (map) => Either.right(map),
     );
   }
 }
