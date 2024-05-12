b9915aa70f4804bcef720689d7020b735652612b
Oscar Ramirez <osramirezf@gmail.com>
2024-05-12 10:18:37 -0400

    feature: Agregamos widget para visualiar los actores por peliculas y disponibilizamos en la vista de MovieContent

---
+++ lib/app/presentation/modules/movie/widgets/movie_cast.dart
@@ -0,0 +1,111 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../core/utils/either.dart';
+import '../../../../domain/failures/http_request/http_request_failure.dart';
+import '../../../../domain/models/peformer/performer.dart';
+import '../../../../domain/repositories/movie_repository.dart';
+import '../../../global/utils/get_image_url.dart';
+import '../../../global/widgets/request_failed.dart';
+
+class MovieCast extends StatefulWidget {
+  const MovieCast({super.key, required this.movieId});
+  final int movieId;
+
+  @override
+  State<MovieCast> createState() => _MovieCastState();
+}
+
+class _MovieCastState extends State<MovieCast> {
+  late Future<Either<HttpRequestFailure, List<Performer>>> _future;
+
+  @override
+  void initState() {
+    super.initState();
+    _initFuture();
+  }
+
+  void _initFuture() {
+    _future = context.read<MovieRepository>().getCatsByMovie(widget.movieId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Either<HttpRequestFailure, List<Performer>>>(
+        key: ValueKey(_future),
+        future: _future,
+        builder: (_, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return snapshot.data!.when(
+            left: (_) => RequestFailed(onRetry: () {
+              setState(() {
+                _initFuture();
+              });
+            }),
+            right: (cast) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Text(
+                      'Cast',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 100,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      separatorBuilder: (_, __) => const SizedBox(width: 6.5),
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      itemCount: cast.length,
+                      itemBuilder: (_, int index) {
+                        final performer = cast[index];
+                        return Column(
+                          children: [
+                            Flexible(
+                              child: LayoutBuilder(
+                                builder: (_, constraints) {
+                                  final size = constraints.maxHeight;
+                                  return ClipRRect(
+                                    borderRadius:
+                                        BorderRadius.circular(size / 2),
+                                    child: ExtendedImage.network(
+                                      getImageUrl(performer.profilePath),
+                                      height: size,
+                                      width: size,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              performer.name,
+                              style: const TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  )
+                ],
+              );
+            },
+          );
+        });
+  }
+}
--- lib/app/presentation/modules/movie/widgets/movie_content.dart
+++ lib/app/presentation/modules/movie/widgets/movie_content.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../global/global.dart';
 import '../controllers/state/movie_state.dart';
+import 'movie_cast.dart';
 import 'movie_header.dart';
 
 class MovieContent extends StatelessWidget {
@@ -17,14 +18,16 @@ class MovieContent extends StatelessWidget {
       children: [
         MovieHeader(movie: movie),
         Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              movie.overview,
-              style: const TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-              ),
-            )),
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            movie.overview,
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        MovieCast(movieId: movie.id),
       ],
     ));
   }
