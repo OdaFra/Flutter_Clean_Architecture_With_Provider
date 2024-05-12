b705b40d9df8410562fbb5829ff59258cb376ae8
Oscar Ramirez <osramirezf@gmail.com>
2024-05-12 12:19:09 -0400

    feature: Agregamos funciones para guardar y obtener el accountId en el secureStorage

--- lib/app/data/services/local/session_service.dart
+++ lib/app/data/services/local/session_service.dart
@@ -1,15 +1,23 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const _key = 'sessionId';
+const _accountKey = 'accountId';
 
 class SessionService {
   SessionService(this._secureStorage);
 
   final FlutterSecureStorage _secureStorage;
 
+  // Para obtener el key de la sessionId
   Future<String?> get sessionId async {
     final sessionId = await _secureStorage.read(key: _key);
     return sessionId;
+  }
+
+//Para obtener el key del accountId
+  Future<String?> get accountId async {
+    final accountKey = await _secureStorage.read(key: _accountKey);
+    return accountKey;
   }
 
   Future<void> saveSessionId(String sessionId) {
@@ -19,7 +27,15 @@ class SessionService {
     );
   }
 
+  Future<void> saveAccountId(String accountId) {
+    return _secureStorage.write(
+      key: _accountKey,
+      value: accountId,
+    );
+  }
+
   Future<void> signOut() {
-    return _secureStorage.delete(key: _key);
+    return _secureStorage.deleteAll();
+    // await _secureStorage.delete(key: _key);
   }
 }
