3bc52ca8602142742ea5585116b283b7022c2140
Oscar Ramirez <osramirezf@gmail.com>
2024-05-26 19:55:41 -0400

    refactor: Adecuamos favorite_view para disponibilizar vista de appbar como contenido.

--- lib/app/presentation/modules/favorites/views/favorite_view.dart
+++ lib/app/presentation/modules/favorites/views/favorite_view.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class FavoriteView extends StatelessWidget {
+import '../../../global/controllers/favorite/favorite_controller.dart';
+import '../../../global/widgets/request_failed.dart';
+import '../widgets/favorite_content.dart';
+import 'favorite_appbar.dart';
+
+class FavoriteView extends StatefulWidget {
   const FavoriteView({super.key});
 
   @override
+  State<FavoriteView> createState() => _FavoriteViewState();
+}
+
+class _FavoriteViewState extends State<FavoriteView>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final controller = context.watch<FavoriteController>();
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 1,
+      appBar: FavoritesAppBar(tabController: _tabController),
+      body: controller.state.map(
+        loading: (_) => const Center(
+          child: CircularProgressIndicator(),
+        ),
+        failed: (_) => RequestFailed(
+          onRetry: () => controller.init(),
+        ),
+        loaded: (state) => FavoriteContent(
+          state: controller,
+          tabController: _tabController,
+        ),
       ),
     );
   }
