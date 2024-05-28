4f91495e6788d7601488011e49af93d179f3638c
Oscar Ramirez <osramirezf@gmail.com>
2024-05-28 16:38:12 -0400

    feat; Agregamos widget FavoritesList para listar las peliculas y series favoritas en el contenido de favoritos

--- lib/app/presentation/modules/favorites/widgets/favorite_content.dart
+++ lib/app/presentation/modules/favorites/widgets/favorite_content.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
-import '../../../global/controllers/favorite/favorite_controller.dart';
+import '../../../global/controllers/favorite/favorite_state.dart';
+import 'favorites_list.dart';
 
 class FavoriteContent extends StatelessWidget {
-  const FavoriteContent({super.key, required this.state, required this.tabController});
-  final FavoriteController state;
+  const FavoriteContent(
+      {super.key, required this.state, required this.tabController});
+  final FavoriteStateLoaded state;
   final TabController tabController;
 
   @override
   Widget build(BuildContext context) {
     return TabBarView(
       controller: tabController,
-      children: const [
-        Text('Movie'),
-        Text('Series'),
+      children: [
+        FavoriteList(items: state.movies.values.toList()),
+        FavoriteList(items: state.series.values.toList())
       ],
     );
   }
---
+++ lib/app/presentation/modules/favorites/widgets/favorites_list.dart
@@ -0,0 +1,65 @@
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../domain/models/media/media.dart';
+import '../../../global/global.dart';
+import '../../../utils/go_to_media_details.dart';
+import '../../movie/views/movie_view.dart';
+
+class FavoriteList extends StatefulWidget {
+  const FavoriteList({super.key, required this.items});
+  final List<Media> items;
+
+  @override
+  State<FavoriteList> createState() => _FavoriteListState();
+}
+
+class _FavoriteListState extends State<FavoriteList>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return ListView.builder(
+      itemCount: widget.items.length,
+      itemBuilder: (_, index) {
+        final item = widget.items[index];
+        return MaterialButton(
+          onPressed: () => goToMediaDetails(context, item),
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              ExtendedImage.network(
+                getImageUrl(
+                  item.posterPath,
+                ),
+                width: 60,
+              ),
+              const SizedBox(width: 10),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item.title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      item.overview,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontSize: 11),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
