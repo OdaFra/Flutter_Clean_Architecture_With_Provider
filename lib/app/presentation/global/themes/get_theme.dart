38d2b9fc03b10ded44fa805c332a7f4bd8b91b9b
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-06-20 17:10:41 -0400

    theme: Agregamos archivo para los colores y adecuamos el de get_theme para el modo dark y light

--- lib/app/presentation/global/themes/get_theme.dart
+++ lib/app/presentation/global/themes/get_theme.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
 
 ThemeData getTheme(bool darkMode) {
+  final themeDark = ThemeData.dark();
+  final themeLight = ThemeData.light();
   if (darkMode) {
-    return ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.grey);
+    return themeDark.copyWith(
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.dark,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      textTheme: GoogleFonts.robotoTextTheme(themeDark.textTheme),
+      scaffoldBackgroundColor: AppColors.darkLight,
+      switchTheme: SwitchThemeData(
+        thumbColor: MaterialStateProperty.all(Colors.grey.shade600),
+        trackColor: MaterialStateProperty.all(Colors.grey.withOpacity(0.2)),
+      ),
+    );
   }
-  return ThemeData.light();
+  return themeLight.copyWith(
+    textTheme: GoogleFonts.robotoTextTheme(
+      themeLight.textTheme.copyWith(
+        bodyMedium: const TextStyle(color: AppColors.dark),
+      ),
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: AppColors.dark,
+      ),
+      titleTextStyle: TextStyle(
+        color: AppColors.dark,
+      ),
+    ),
+    tabBarTheme: const TabBarTheme(labelColor: AppColors.dark),
+  );
 }
